84aba4e4a9150192b5e60dee72fa1b5b95ed3ed3
Francisco Martin <francisco.martin@ambevtech.com.br>
2022-01-02 21:02:35 -0300

    connect with metamask

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 function Login() {
+  const { authenticate } = useMoralis();
+
   return (
     <div className="bg-black relative">
       <h1>Logo Screen</h1>
@@ -11,7 +14,10 @@ function Login() {
           height={200}
           width={200}
         />
-        <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">
+        <button
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+          onClick={authenticate}
+        >
           Login to the METAVERSE
         </button>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
-import Head from 'next/head'
-import Login from '../components/Login'
+import Head from "next/head";
+import Login from "../components/Login";
+import { useMoralis } from "react-moralis";
 
 export default function Home() {
-  const isAuthenticated = false;
+  const { isAuthenticated, logout } = useMoralis();
 
-  if(!isAuthenticated) return <Login />
+  if (!isAuthenticated) return <Login />;
   return (
     <div className="h-screen">
       <Head>
         <title>Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Login />
+      <h1>Welcome Metaverse</h1>
+      <button
+        className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+        onClick={logout}
+      >
+        Logout
+      </button>
     </div>
-  )
+  );
 }
