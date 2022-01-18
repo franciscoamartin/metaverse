5f5fc756fb1109db0fc3c141917fac6bf67f0493
Francisco Martin <francisco.martin@ambevtech.com.br>
2022-01-17 23:51:05 -0300

    chat complete

--- components/Messages.js
+++ components/Messages.js
@@ -1,10 +1,27 @@
-import { ByMoralis, useMoralis } from "react-moralis";
+import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
 import { useRef } from "react";
 import SendMessage from "./SendMessage";
+import Message from "./Message";
 
+const MINS_DURATION = 15;
 function Messages() {
   const { user } = useMoralis();
   const endOfMessagesRef = useRef(null);
+
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true,
+    }
+  );
 
   return (
     <div className="pb-56">
@@ -14,10 +31,14 @@ function Messages() {
           style={{ marginLeft: "auto", marginRight: "auto" }}
         />
       </div>
-      <div></div>
+      <div className="space-y-10 p-4">
+        {data.map((message) => (
+          <Message key={message.key} message={message} />
+        ))}
+      </div>
 
       <div className="flex justify-center">
-        <SendMessage endOfMessagesRef={endOfMessagesRef}/>
+        <SendMessage endOfMessagesRef={endOfMessagesRef} />
       </div>
       <div ref={endOfMessagesRef} className="text-center text-gray-400 mt-5">
         <p>You're up to date {user.getUsername()}!</p>
